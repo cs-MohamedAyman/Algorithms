2b0857186aee529e02595396b7ae27278b65baef
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-03-30 18:21:36 +0200

    Update Longest-Common-Subsequence.py

--- Lectures/13-Dynamic-Programming-I/Longest-Common-Subsequence.py
+++ Lectures/13-Dynamic-Programming-I/Longest-Common-Subsequence.py
@@ -16,6 +16,16 @@ def lcs(X, Y, m, n):
         memo[m][n] = max(choice1, choice2)
         return memo[m][n]
 
+def solution(X, Y, m, n):
+    if m == -1 or n == -1:
+        return ""
+    if X[m] == Y[n]:
+        return solution(X, Y, m-1, n-1) + X[m]
+    if memo[m-1][n] > memo[m][n-1]:
+        return solution(X, Y, m-1, n)
+    else:
+        return solution(X, Y, m, n-1)
+
 dp = [[-1 for i in range(N)] for j in range(N)]
 
 def lcs_iterative(X, Y, m, n):
@@ -29,34 +39,25 @@ def lcs_iterative(X, Y, m, n):
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     return dp[m][n]
 
-def print_solution(X, Y, m, n):
-    # Following code is used to print LCS
+def solution_iterative(X, Y, m, n):
     res = dp[m][n]
-    # Create a character array to store the lcs string
     lcs_res = [''] * res
-    # Start from the right-most-bottom-most corner and 
-    # one by one store characters in lcs[] 
     i, j = m, n
     while i > 0 and j > 0:
-        # If current character in X and Y are same, then current character is part of LCS 
         if X[i-1] == Y[j-1]:
-            # Put current character in result
             lcs_res[res-1] = X[i-1]
-            # reduce values of i, j and index
-            i -= 1
-            j -= 1
-            res -= 1
-        # If not same, then find the larger of two and go in the direction of larger value 
-        elif dp[i-1][j] > dp[i][j-1]:
+            i, j, res = i - 1, j - 1, res - 1
+        elif dp[i-1][j] > dp[i][j-1]: 
             i -= 1
         else:
             j -= 1
-    print("LCS of", X, "and", Y, "is", ''.join(lcs_res))
+    return ''.join(lcs_res)
 
 X = "AGGTAB"
 Y = "GXTXAYB"
 m = len(X)
 n = len(Y)
-print("(Recursive) Length of LCS is", lcs(X, Y, m-1, n-1))
-print("(Iterative) Length of LCS is", lcs_iterative(X, Y, m, n))
-print_solution(X, Y, m, n)
+print("(Recursive) Length of LCS is %d" % lcs(X, Y, m-1, n-1))
+print("(Recursive) LCS of %s and %s is %s" % (X, Y, solution(X, Y, m-1, n-1)))
+print("(Iterative) Length of LCS is %d" % lcs_iterative(X, Y, m, n))
+print("(Iterative) LCS of %s and %s is %s" % (X, Y, solution_iterative(X, Y, m, n)))
