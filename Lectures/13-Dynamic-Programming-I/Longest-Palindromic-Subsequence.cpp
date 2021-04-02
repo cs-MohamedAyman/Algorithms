d5966f8e049db91c44ac5530896b551c9d699b07
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-02 12:54:27 +0200

    Create Longest-Palindromic-Subsequence.cpp

---
+++ Lectures/13-Dynamic-Programming-I/Longest-Palindromic-Subsequence.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int const N = 1e3+3;
+
+int memo[N][N];
+
+int longest_palindrome(string s, int i, int j) {
+    if (i > j)
+        return 0;
+    if (i == j)
+        return 1;
+	if (memo[i][j] != -1)
+		return memo[i][j];
+    if (s[i] == s[j]) {
+        memo[i][j] = longest_palindrome(s, i+1, j-1) + 2;
+		return memo[i][j];
+	}
+	int choice1 = longest_palindrome(s, i, j-1);
+	int choice2 = longest_palindrome(s, i+1, j);
+    memo[i][j] = max(choice1, choice2);
+	return memo[i][j];
+}
+
+int dp[N][N];
+
+int longest_palindrome_iterative(string s, string t, int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[n][n];
+}
+
+int main() {
+    int n = 8;
+    string s = "ABBDCACB";
+	string t = s;
+	reverse(t.begin(), t.end());
+    memset(memo, -1, sizeof memo);
+    cout << "(Recursive) Longest Palindrome Length is " << longest_palindrome(s, 0, n-1) << '\n';
+    cout << "(Iterative) Longest Palindrome Length is " << longest_palindrome_iterative(s, t, n) << '\n';
+}
