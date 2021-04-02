b02c7f35f2014ed0b74c6cd26d7133d812b491a9
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-02 12:56:01 +0200

    Update Longest-Palindromic-Subsequence.cpp

--- Lectures/13-Dynamic-Programming-I/Longest-Palindromic-Subsequence.cpp
+++ Lectures/13-Dynamic-Programming-I/Longest-Palindromic-Subsequence.cpp
@@ -25,9 +25,11 @@ int longest_palindrome(string s, int i, int j) {
 int dp[N][N];
 
 int longest_palindrome_iterative(string s, string t, int n) {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (s[i-1] == t[j-1])
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+			if (i == 0 || j == 0)
+				dp[i][j] = 0;
+            else if (s[i-1] == t[j-1])
                 dp[i][j] = dp[i-1][j-1] + 1;
             else
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
