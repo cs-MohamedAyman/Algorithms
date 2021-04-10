ee7cae9f6bf197e1c3055dde2fec58aef9bbf87f
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-11 01:58:09 +0200

    Update Efficient-Huffman-Coding.py

--- Lectures/11-Greedy-Algorithms/Efficient-Huffman-Coding.py
+++ Lectures/11-Greedy-Algorithms/Efficient-Huffman-Coding.py
@@ -18,18 +18,36 @@ def print_nodes(node, val = ''):
         print_nodes(node.right, new_val)
     if (not node.left and not node.right):
         print(node.symbol, "->", new_val)
+        codes[node.symbol] = new_val
  
-chars = ['a', 'b', 'c', 'd', 'e', 'f']
-freq  = [ 5,   9,   12,  13,  16, 45 ]
-nodes = []
-for i in range(len(chars)):
-    nodes.append(node(freq[i], chars[i]))
-heapq.heapify(nodes)
-while len(nodes) > 1:
-    left = heapq.heappop(nodes)
-    right = heapq.heappop(nodes)
-    left.huff = 0
-    right.huff = 1
-    new_node = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
-    heapq.heappush(nodes, new_node)
-print_nodes(heapq.heappop(nodes))
+def huffman_codes(chars, freq):
+    nodes = []
+    for i in range(len(chars)):
+        nodes.append(node(freq[i], chars[i]))
+    heapq.heapify(nodes)
+    while len(nodes) > 1:
+        left = heapq.heappop(nodes)
+        right = heapq.heappop(nodes)
+        left.huff = 0
+        right.huff = 1
+        new_node = node(left.freq+right.freq, 
+                        left.symbol+right.symbol, 
+                        left, 
+                        right)
+        heapq.heappush(nodes, new_node)
+    root = heapq.heappop(nodes)
+    return root
+
+def encoding(codes):
+    encoded_string = ''
+    for i in s: encoded_string += codes[i]
+    return encoded_string
+
+s = "aafffafffcccaffffbeeefffffbbcccfffffccceeeefffdddeeeeeffccffddfffbbbbbbdddafffffddddeeeeffffcdffffff"
+chars = list(set(s))
+freq  = [s.count(i) for i in chars]
+root = huffman_codes(chars, freq)
+codes = {}
+print_nodes(root)
+encoded_string = encoding(codes)
+print('Encoded huffman data:\n' + encoded_string)
