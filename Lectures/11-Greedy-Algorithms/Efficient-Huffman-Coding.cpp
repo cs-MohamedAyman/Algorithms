605ca4b8f6650b7b10f2aec1c2eb0b81bca35ef6
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-11 12:00:31 +0200

    Update Efficient-Huffman-Coding.cpp

--- Lectures/11-Greedy-Algorithms/Efficient-Huffman-Coding.cpp
+++ Lectures/11-Greedy-Algorithms/Efficient-Huffman-Coding.cpp
@@ -17,24 +17,27 @@ struct node {
     }
 };
 
-void print_nodes(node* curr, string val = "") {
+void print_nodes(node* curr, map<string, string> &codes, string val = "") {
     string new_val = val + curr->huff;
     if (curr->left)
-        print_nodes(curr->left, new_val);
+        print_nodes(curr->left, codes, new_val);
     if (curr->right)
-        print_nodes(curr->right, new_val);
-    if (!curr->left && !curr->right)
+        print_nodes(curr->right, codes, new_val);
+    if (!curr->left && !curr->right) {
         cout << curr->symbol << " -> " << new_val << '\n';
+		string curr_symbol = "";
+		curr_symbol += curr->symbol;
+		codes[curr_symbol] = new_val;
+	}
 }
 
-int main() {
-    int n = 6;
-	string chars[] = {"a", "b", "c", "d", "e", "f"};
-	int freq[]     = { 5,   9,   12,  13,  16, 45 };
+node* huffman_codes(map<char, int> freq) {
 	auto comp = [](auto& x, auto& y){return x->freq > y->freq;};
 	priority_queue<node*, vector<node*>, decltype(comp)> nodes(comp);
-	for (int i = 0; i < n; i++) {
-        node* curr = new node(freq[i], chars[i]);
+	for (auto i : freq) {
+		string curr_symbol = "";
+		curr_symbol += i.first;
+        node* curr = new node(i.second, curr_symbol);
 		nodes.push(curr);
     }
 	while (nodes.size() > 1) {
@@ -44,8 +47,33 @@ int main() {
 		nodes.pop();
 		left->huff = "0";
 		right->huff = "1";
-		node* new_node = new node(left->freq+right->freq, left->symbol+right->symbol, left, right);
+		node* new_node = new node(left->freq+right->freq, 
+								  left->symbol+right->symbol, 
+								  left, 
+								  right);
 		nodes.push(new_node);
 	}
-	print_nodes(nodes.top());
+	node* root = nodes.top();
+    return root;
+}
+
+string encoding(string s, map<string, string> &codes) {
+    string encoded_string = "";
+    for (int i = 0; i < s.size(); i++) {
+		string curr_si = "";
+		curr_si += s[i];
+		encoded_string += codes[curr_si];
+	}
+    return encoded_string;
+}
+
+int main() {
+	string s = "aafffafffcccaffffbeeefffffbbcccfffffccceeeefffdddeeeeeffccffddfffbbbbbbdddafffffddddeeeeffffcdffffff";
+    map<char, int> freq;
+	for (auto i : s) freq[i] ++;
+	node* root = huffman_codes(freq);
+	map<string, string> codes;
+	print_nodes(root, codes);
+    string encoded_string = encoding(s, codes);
+	cout << "Encoded huffman data:\n" << encoded_string << '\n';
 }
